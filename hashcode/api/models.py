b6d2b47d996ec00daa093ea4ef197a1f5d3f4fed
Toon Baeyens <toon.baeyens@gmail.com>
2020-02-20 19:48:25 +0100

    automatic books from, lib

--- hashcode/api/models.py
+++ hashcode/api/models.py
@@ -4,16 +4,28 @@ class Library():
         self.num_books = num_books
         self.num_days = num_days
         self.flow = flow
-        self.books = books
+        self.books = set(books)
 
 
 class Solution():
 
-    def __init__(self):
+    def __init__(self, problem):
         self.libraries = []
+        self.problem = problem
+        self.found_books = set()
+        self.days_left = problem.num_days
 
-    def add_lib(self, id_, books):
-        self.libraries.append(SolLib(id_, books))
+    def add_lib(self, lib, books=None):
+        self.days_left -= lib.num_days
+        if books == None:
+            bbw = [(self.problem.weights[book], books) for book in lib.books if book not in self.found_books]
+            bbw.sort()
+            bbw.reverse()
+            books = list(zip(*bbw))[1][:lib.flow*self.days_left]
+
+        for book in books:
+            self.found_books.add(book)
+        self.libraries.append(SolLib(lib.id, books))
 
     def print(self):
         num_of_libraries = 0
