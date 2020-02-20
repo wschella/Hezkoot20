8d687d623782a2593bd896acd4152826437016d2
Toon Baeyens <toon.baeyens@gmail.com>
2020-02-20 19:53:36 +0100

    small fix

--- hashcode/api/models.py
+++ hashcode/api/models.py
@@ -18,7 +18,7 @@ class Solution():
     def add_lib(self, lib, books=None):
         self.days_left -= lib.num_days
         if books == None:
-            bbw = [(self.problem.weights[book], books) for book in lib.books if book not in self.found_books]
+            bbw = [(self.problem.weights[book], book) for book in lib.books if book not in self.found_books]
             bbw.sort()
             bbw.reverse()
             books = list(zip(*bbw))[1][:lib.flow*self.days_left]
